4aa321340f45fdc7d2149f9d04928020946dc6eb
Rica Feng <topgb@msn.com>
2016-01-10 02:09:37 -0800

    Updated Entity

--- Retriever/Retriever/Entity.cpp
+++ Retriever/Retriever/Entity.cpp
@@ -1,5 +1,24 @@
-#include "stdafx.h"
 #include "Entity.h"
 
+using namespace std;
 
+//----------------------------PUBLIC----------------------------//
 
+/*Return entity name*/
+string Entity::get_Name() const { return name; }
+/*Change entity name*/
+void Entity::set_Name(const std::string v) { name = v; }
+
+/*Return entity description*/
+std::string Entity::get_Description() const { return description; }
+/*Change entity descriptionn*/
+void Entity::set_Description(const std::string v) { description = v; }
+
+/*Returns entity ID*/
+unsigned int Entity::get_Id() const { return id; }
+/*Change entity ID*/
+void Entity::set_Id(unsigned int v) { id = v; }
+
+//----------------------------PROTECTED----------------------------//
+
+//----------------------------PRIVATE----------------------------//
--- Retriever/Retriever/Entity.h
+++ Retriever/Retriever/Entity.h
@@ -1,20 +1,34 @@
 #pragma once
 
-struct Position
-{
-	float x;
-	float y;
-};
+#include <string>
 
+//Game object that is displayed on screen
 class Entity
 {
 public:
-	Entity() {};
-	//set to true if object is collidable
-	bool collidable;
-	//top left corner
-	Position posBegin;
-	//bottom right corner
-	Position posEnd;
+	/*Return entity name*/
+	std::string get_Name() const;
+	/*Change entity name*/
+	void set_Name(const std::string v);
+
+	/*Return entity description*/
+	std::string get_Description() const;
+	/*Change entity descriptionn*/
+	void set_Description(const std::string v);
+
+	/*Returns entity ID*/
+	unsigned int get_Id() const;
+	/*Change entity ID*/
+	void set_Id(unsigned int v);
+
+protected:
+	//Name of entity
+	std::string name;
+
+	//Description of entity
+	std::string description;
+
+	//ID of entity
+	unsigned int id;
 };
 
